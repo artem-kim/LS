1f3de53cb7405c62d0d167abcea425ef90046a98
Artem Kim <artemkimlondon@gmail.com>
2020-08-23 10:53:23 +0100

    first commit

---
+++ JS101/lesson2/mortgage.js
@@ -0,0 +1,54 @@
+const readline = require('readline-sync');
+let answer;
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' ||
+         Number.isNaN(Number(number)) ||
+         Number(number) < 0;
+}
+
+prompt('Welcome to Mortgage Calculator!');
+
+do {
+  prompt("What's the loan amount in dollars?");
+  let loanAmount = readline.question();
+
+  while (invalidNumber(loanAmount)) {
+    prompt("Please enter a valid amount.");
+    loanAmount = readline.question();
+  }
+
+  prompt("What's the Annual Percentage Rate (APR)?
+         For example, enter '5%' as 5.");
+  let apr = readline.question();
+
+  while (invalidNumber(apr)) {
+    prompt("Please enter a valid rate.");
+    apr = readline.question();
+  }
+
+  prompt("What's the loan duration in months?");
+  let durationMonths = readline.question();
+
+  while (invalidNumber(durationMonths)) {
+    prompt("Please enter a valid number of months.");
+    durationMonths = readline.question();
+  }
+
+  let monthlyRate = apr / 100 / 12;
+  let monthlyPayment = loanAmount * (monthlyRate /
+                       (1 - Math.pow((1 + monthlyRate), (-durationMonths))));
+
+  console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
+
+  prompt("Another calculation?");
+  answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+} while (answer === "y");
