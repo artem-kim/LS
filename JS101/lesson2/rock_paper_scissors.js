4925d9a118251f08ada8ead0620ea0f7f1c4a26e
Artem Kim <artemkimlondon@gmail.com>
2020-08-27 17:58:44 +0100

    Fix issues after code review

--- JS101/lesson2/rock_paper_scissors.js
+++ JS101/lesson2/rock_paper_scissors.js
@@ -1,61 +1,87 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-const ABBREVIATED_CHOICES =
-{r: 'rock', p: 'paper', sc: 'scissors', l: 'lizard', sp: 'spock'};
-let score = 0;
-let computerScore = 0;
+const SCORE = {userScore: 0, computerScore: 0};
 const MAX_SCORE = 5;
+const ABBREVIATED_CHOICES = {
+  r: 'rock', p: 'paper', sc: 'scissors', l: 'lizard', sp: 'spock'
+};
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
+
 
 // Main programme
 while (true) {
   console.clear();
 
-  prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!');
-  displayChoices();
-  let abbreviatedChoice = readline.question();
+  prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
+  prompt('First to five wins the game.\n');
 
-  // Check for valid choice
-  while (!ABBREVIATED_CHOICES.hasOwnProperty(abbreviatedChoice)) {
-    prompt('Please enter a valid choice');
-    abbreviatedChoice = readline.question();
-  }
+  let choice = retrieveUserChoice();
 
-  let choice = ABBREVIATED_CHOICES[abbreviatedChoice];
+  let computerChoice = retrieveComputerChoice();
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
   displayWinner(choice, computerChoice);
 
-  computeScore(choice, computerChoice);
-  prompt(`Your score is ${score}. Computer's score is ${computerScore}.`);
-  let oneMoreGame;
+  computeScore(choice, computerChoice, SCORE);
 
-  if (score < MAX_SCORE && computerScore < MAX_SCORE) {
+  displayScore(SCORE);
+
+  let oneMoreGame = isMatchOver(SCORE);
+
+  if (oneMoreGame === 'n') break;
+}
+
+displayGrandWinner(SCORE);
+
+function displayGrandWinner(score) {
+  if (score.userScore > score.computerScore) {
+    prompt('Game over. You are a grand winner!');
+  } else if (score.computerScore > score.userScore) {
+    prompt('Game over. Computer is a grand winner!');
+  } else {
+    prompt("Game over. It's a tie.");
+  }
+}
+
+function isMatchOver(score) {
+  if (score.userScore < MAX_SCORE && score.computerScore < MAX_SCORE) {
     prompt('Do you want to play one more game? (y/n)');
     oneMoreGame = readline.question().toLowerCase();
   } else {
     oneMoreGame = 'n';
   }
 
-  while (oneMoreGame[0] !== 'n' && oneMoreGame[0] !== 'y') {
+  while (oneMoreGame !== 'n' && oneMoreGame !== 'y') {
     prompt('Please enter "y" or "n".');
     oneMoreGame = readline.question().toLowerCase();
   }
-  if (oneMoreGame[0] === 'n') break;
+  return oneMoreGame;
 }
 
-// final message
-if (score > computerScore) {
-  prompt('Game over. You are a grand winner!');
-} else if (computerScore > score) {
-  prompt('Game over. Computer is a grand winner!');
-} else {
-  prompt("Game over. It's a tie.");
+function retrieveUserChoice() {
+  displayChoices();
+
+  let abbreviatedChoice = readline.question().toLowerCase();
+
+  while (!ABBREVIATED_CHOICES.hasOwnProperty(abbreviatedChoice)) {
+    prompt('Please enter a valid choice');
+    abbreviatedChoice = readline.question().toLowerCase();
+  }
+  let choice = ABBREVIATED_CHOICES[abbreviatedChoice];
+  return choice;
 }
 
-// Function that displays message with choices
+function retrieveComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+  return computerChoice;
+}
+
 function displayChoices() {
   prompt('Choose one:');
   for (let index = 0; index < VALID_CHOICES.length; index++) {
@@ -71,56 +97,35 @@ function displayChoices() {
   }
 }
 
+function displayScore(score) {
+  prompt(`Your score is ${score.userScore}. Computer's score is ${score.computerScore}.`);
+}
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function computeScore(choice, computerChoice) {
+function computeScore(choice, computerChoice, score) {
   if (checkIfWin(choice, computerChoice)) {
-    score += 1;
-  } else if (checkIfLose(choice, computerChoice)) {
-    computerScore += 1;
+    score.userScore += 1;
+  } else if (choice === computerChoice) {
+    score.userScore += 0;
+  } else {
+    score.computerScore += 1;
   }
 }
 
 function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
   if (checkIfWin(choice, computerChoice)) {
     prompt('You win!');
-  } else if (checkIfLose(choice, computerChoice)) {
-    prompt('Computer wins!');
-  } else {
+  } else if (choice === computerChoice) {
     prompt("It's a tie!");
+  } else {
+    prompt('Computer wins!');
   }
 }
 
 function checkIfWin(choice, computerChoice) {
-  if ((choice === 'rock' && (computerChoice === 'scissors' ||
-  computerChoice === 'lizard')) ||
-      (choice === 'paper' && (computerChoice === 'rock' ||
-      computerChoice === 'spock')) ||
-      (choice === 'scissors' && (computerChoice === 'paper' ||
-      computerChoice === 'lizard')) ||
-      (choice === 'lizard' && (computerChoice === 'spock' ||
-      computerChoice === 'paper')) ||
-      (choice === 'spock' && (computerChoice === 'rock' ||
-      computerChoice === 'scissors'))) {
-    return true;
-  }
-  return false;
-}
-
-function checkIfLose(choice, computerChoice) {
-  if ((choice === 'rock' && (computerChoice === 'paper' ||
-  computerChoice === 'spock')) ||
-             (choice === 'paper' && (computerChoice === 'scissors' ||
-             computerChoice === 'lizard')) ||
-             (choice === 'scissors' && (computerChoice === 'rock' ||
-             computerChoice === 'spock')) ||
-             (choice === 'lizard' && (computerChoice === 'rock' ||
-             computerChoice === 'scissors')) ||
-             (choice === 'spock' && (computerChoice === 'lizard' ||
-             computerChoice === 'paper'))) {
-    return true;
-  }
-  return false;
+  return WINNING_COMBOS[choice].includes(computerChoice);
 }
