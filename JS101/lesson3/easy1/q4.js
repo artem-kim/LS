3b57a62d8bf880a0400e5c01bb579aa52d9ce1d5
Artem Kim <artemkimlondon@gmail.com>
2020-08-27 17:59:59 +0100

    Move to separate folder

---
+++ JS101/lesson3/easy1/q4.js
@@ -0,0 +1,4 @@
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+
+let newString = munstersDescription[0].toUpperCase() +
+                munstersDescription.substring(1).toLowerCase();
